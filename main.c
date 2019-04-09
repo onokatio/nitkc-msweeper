1d898d55ec5ffcddfc0d77b9e7c36e76a9529843
onokatio <onokatio@gmail.com>
2019-04-09 15:39:02 +0900

    show, open

--- main.c
+++ main.c
@@ -13,8 +13,10 @@
 int main(void){
 
 	int ms[sizeX][sizeY];
+	int isOpen[sizeX][sizeY];
 
 	init(ms);
+	init(isOpen);
 
 	setRandom(ms);
 
@@ -24,7 +26,7 @@ int main(void){
 
 	dump(ms);
 
-	start(ms);
+	start(ms,isOpen);
 
 	return 0;
 
@@ -94,10 +96,67 @@ void prompt(int *x,int *y){
 	scanf("%d %d",x,y);
 }
 
-int start(int ms[sizeX][sizeY]){
+void show(int ms[sizeX][sizeY],int isOpen[sizeX][sizeY]){
+	int i,j;
+	for(j=0;j<sizeY;j++){
+		for(i=0;i<sizeX;i++){
+			if(isOpen[i][j] == 1){
+				if(ms[i][j] == -1){
+					printf("%d",ms[i][j]);
+				}else{
+					printf("M");
+				}
+			}else{
+				printf("*");
+			}
+			printf(" ");
+		}
+		printf("\n");
+	}
+}
+
+int calcFail(int inputX, inputY, int ms[sizeX][sizeY],int isOpen[sizeX][sizeY]){
+	if(isOpen[inputX][inputY] ==1) return 2;
+
+	if(ms[inputX][inputY] == -1){
+		return 1;
+	}
+
+	return 0;
+}
+
+int calcOpen(int inputX, inputY, int ms[sizeX][sizeY],int isOpen[sizeX][sizeY]){
+	if(x != 0)         if(ms[xLeft][y]   == -1) count++;
+	if(x != sizeX - 1) if(ms[xRight][y]  == -1) count++;
+	if(y != 0)         if(ms[x][yTop]    == -1) count++;
+	if(y != sizeY - 1) if(ms[x][yBottom] == -1) count++;
+
+	if(x != 0         && y != 0)         if(ms[xLeft][yTop]     == -1) count++;
+	if(x != sizeY - 1 && y != 0)         if(ms[xRight][yTop]    == -1) count++;
+	if(x != 0         && y != sizeY - 1) if(ms[xLeft][yTop]     == -1) count++;
+	if(x != sizeY - 1 && y != sizeY - 1) if(ms[xRight][yBottom] == -1) count++;
+}
+
+void start(int ms[sizeX][sizeY],int isOpen[sizeX][sizeY]){
 	int inputX,inputY;
+	int loop = 1;
+	int failReturn;
 
-	prompt(&inputX,&inputY);
+	while(loop){
+		show(ms,isOpen);
 
-	printf("ms[%d][%d] = %d",inputX,inputY, ms[inputX][inputY]);
+		prompt(&inputX,&inputY);
+		//printf("ms[%d][%d] = %d",inputX,inputY, ms[inputX][inputY]);
+
+		failReturn = calcFail(inputX, inputY, ms, isOpen);
+
+		if(failReturn == 1){
+			loop = 0;
+		}else if(calcReturn == 2){
+			printf("The position is already opened.");
+		}else{
+			isOpen[inputX][inputY] = 0;
+			calcOpen(inputX, inputY, ms,isOpen);
+		}
+	}
 }
